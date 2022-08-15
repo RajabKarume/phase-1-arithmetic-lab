015918b9f8e5f60c6fcb7e1d65a83ccbd5a26f2b
Rajab <rkarume@gmail.com>
2022-08-15 22:51:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let random = Math.random().toFixed(1) *10;
+ let num1 = 31;
+ let num2 = 2;
 
-// Write your code here
+ const multiply = num1*num2;
+
+ let num3 = 20;
+ let num4 = 8;
+
+ const mod = num3 %num4;
+
+ const max = Math.max(13, 16, 20, 3, 2, 5, 10, 14);
